d9861bea22dcf28b06383d11aee32738af5f4589
Алиса Аниськина <alisa.aniskina@gmail.com>
2022-08-17 09:42:10 +0600

    Добавлены тесты

--- src/test/java/soapquadraticequation/soap/servises/QuadraticEquationSolutionServiceTest.java
+++ src/test/java/soapquadraticequation/soap/servises/QuadraticEquationSolutionServiceTest.java
@@ -1,4 +1,37 @@
-import static org.junit.jupiter.api.Assertions.*;
+package soapquadraticequation.soap.servises;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class QuadraticEquationSolutionServiceTest {
-  
+
+    @Test
+    void getWithoutSolution() {
+
+        QuadraticEquationSolution solution = QuadraticEquationSolutionService.getSolution(1, 2, 3);
+        Assertions.assertEquals(solution.getD(), -8);
+        Assertions.assertEquals(solution.getX1(), null);
+        Assertions.assertEquals(solution.getX2(), null);
+    }
+
+    @Test
+    void getOneSolution() {
+        QuadraticEquationSolution solution = QuadraticEquationSolutionService.getSolution(1, -6, 9);
+        Assertions.assertEquals(solution.getD(), 0);
+        Assertions.assertEquals(solution.getX1(),  3);
+        Assertions.assertEquals(solution.getX2(), null);
+
+
+
+    }
+
+
+    @Test
+    void getTwoSolution() {
+        QuadraticEquationSolution solution = QuadraticEquationSolutionService.getSolution(1, -4, -5);
+        Assertions.assertEquals(solution.getD(), 36);
+        Assertions.assertEquals(solution.getX1(),  5);
+        Assertions.assertEquals(solution.getX2(), -1);
+
+    }
 }
